1a084ab554ca71967e850330939e30d485329c6f
nhutb2203520 <nhutb2203520@student.ctu.edu.vn>
2025-07-25 08:12:44 +0700

    capnhat contactadd

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -15,7 +15,7 @@
                 <button class="btn btn-sm btn-primary" @click="refreshList()">
                     <i class="fas fa-redo"></i> Làm mới
                 </button>
-                <button class="btn btn-sm btn-success" @click="goToAddContact">
+                <button class="btn btn-sm btn-success" @click="goToAddContact()">
                     <i class="fas fa-plus"></i> Thêm mới
                 </button>
                 <button class="btn btn-sm btn-danger" @click="removeAllContacts">
@@ -31,15 +31,15 @@
                 </h4>
                 <ContactCard :contact="activeContact" />
                 <router-link
-            :to="{
-            name: 'contact.edit',
-            params: { id: activeContact._id },
-            }"
-            >
-            <span class="mt-2 badge badge-warning">
-            <i class="fas fa-edit"></i> Hiệu chỉnh</span
-            >
-            </router-link>
+                    :to="{
+                    name: 'contact.edit',
+                    params: { id: activeContact._id },
+                    }"
+                    >
+                    <span class="mt-2 badge badge-warning">
+                    <i class="fas fa-edit"></i> Hiệu chỉnh</span
+                    >
+                    </router-link>
             </div>
         </div>
     </div>
@@ -117,16 +117,18 @@ async removeAllContacts() {
             }
         }
     },
-
-},
 goToAddContact() {
     this.$router.push({ name: "contact.add" });
 },
 
+
+
+},
 mounted() {
     this.refreshList();
     
 },
+
 };
 </script>
 <style scoped>
